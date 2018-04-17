5ebc01266c6de6a6da96a882a8fd1fea4401aa6a
fozoglu <f.ozoglu@gmail.com>
2018-04-17 11:36:11 +0300

    added unit test class

--- KMeans-MapKit/KMeans-MapKitTests/KMeans_MapKitTests.swift
+++ KMeans-MapKit/KMeans-MapKitTests/KMeans_MapKitTests.swift
@@ -7,30 +7,42 @@
 //
 
 import XCTest
+import MapKit
 @testable import KMeans_MapKit
 
 class KMeans_MapKitTests: XCTestCase {
     
+    var viewControllerUnderTest : ViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+       
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAnnotationsCount() {
+        
+        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as? ViewController else{
+            XCTFail("Could not instatiate VC from Main Storyboard")
+            return
         }
+        vc.loadViewIfNeeded()
+        guard let mapView = vc.mapView else {
+            XCTFail("ViewController should have outlet set for mapView")
+            return
+        }
+        XCTAssertEqual(mapView.annotations.count, 0, "Maps has not annotations.")
+        
     }
+    
+    func testMapInitialization() {
+        XCTAssert(viewControllerUnderTest.mapView.mapType == MKMapType.standard);
+    }
+    
     
 }
